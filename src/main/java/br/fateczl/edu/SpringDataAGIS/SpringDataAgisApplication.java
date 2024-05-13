af303e79a5189b18d51263fb3212bffcec404b17
luiz <luiz@DESKTOP-JMRQAN7>
2024-05-13 15:26:45 -0300

    commit inicial

---
+++ src/main/java/br/fateczl/edu/SpringDataAGIS/SpringDataAgisApplication.java
@@ -0,0 +1,13 @@
+package br.fateczl.edu.SpringDataAGIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAgisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAgisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fateczl/edu/SpringDataAGIS/SpringDataAgisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fateczl.edu.SpringDataAGIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAgisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
